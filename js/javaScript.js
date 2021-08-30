367d4e3fa01ec3e65c766d0acee54afe35387543
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-08-29 22:47:28 -0700

    initial setup complete, next is score and timer

--- js/javaScript.js
+++ js/javaScript.js
@@ -7,14 +7,19 @@
 // Use of const vs var - https://www.w3schools.com/js/js_const.asp
 
 const startButton = document.getElementById('start-btn')
+const submitButton = document.getElementById('submit-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 
 startButton.addEventListener('click', startQuiz)
+submitButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startQuiz() {
     console.log('Started')
@@ -22,7 +27,7 @@ function startQuiz() {
     shuffledQuestions = questions.sort(() => Math.random() - .5 )
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
-    setNExtQuestion()
+    setNextQuestion()
     // ! start timer should initiate here
     // ! should I have a time for the whole thing or a timer for the whole thing
     // ! or both?
@@ -31,27 +36,96 @@ function startQuiz() {
 
 }
 
-function setNExtQuestion () {
+function setNextQuestion () {
+    console.log('Next Question')
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+      }  
+      )
 }
 
-function selectAnswer() {
+function resetState() {
+    console.log('resetState')
+    clearStatusClass(document.body)
+    submitButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 
+}
+
+function selectAnswer(e) {
+    const selectButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    submitButton.classList.remove('hide')
+    } else {
+        alert('Your score is: + score amount')
+        alert('You have completed the quiz, if you would like to do it again, hit restart!')
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
 {
-    question: 'quesiton goes here',
+    question: 'question 1  goes here',
+    answers: [
+        {text: 'correct 1  answer', correct: true }, //true is specific to the correct answer
+        {text: 'incorrect 2 answer', correct: false }, 
+        {text: 'incorrect 3  answer', correct: false },
+        {text: 'incorrect 4 answer', correct: false }
+    ]
+},
+
+{
+    question: 'question 2 goes here',
     answers: [
         {text: 'correct answer', correct: true }, //true is specific to the correct answer
-        {text: 'incorrect answer', correct: false }, 
-        {text: 'incorrect answer', correct: false },
-        {text: 'incorrect answer', correct: false }
+        {text: 'incorrect answer1', correct: false }, 
+        {text: 'incorrect answer2', correct: false },
+        {text: 'incorrect answer3', correct: false }
+    ]
+},
+{
+    question: 'quesiton 3  goes here',
+    answers: [
+        {text: 'correct answer', correct: true }, //true is specific to the correct answer
+        {text: 'incorrect answer 4', correct: false }, 
+        {text: 'incorrect answer 5', correct: false },
+        {text: 'incorrect answer 6', correct: false }
     ]
 }
-
 ]
